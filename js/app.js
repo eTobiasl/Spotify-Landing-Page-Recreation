2ef31bbb312c6e191ee173a03c686f94027a28f4
Erik Tobias Leirvik <58857842+eTobiasl@users.noreply.github.com>
2020-09-23 15:41:50 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
-let menu = document.getElementById("menu");
-let navList = document.getElementById("nav-list");
-let menuTop = document.getElementById("menu-top");
-let menuMiddle = document.getElementById("menu-middle");
-let menuBottom = document.getElementById("menu-bottom");
+const menu = document.getElementById("menu");
+const navSmall = document.getElementById("menu-small");
+const menuTop = document.getElementById("menu-top");
+const menuMiddle = document.getElementById("menu-middle");
+const menuBottom = document.getElementById("menu-bottom");
+const decoration = document.getElementById("decoration");
+
+
 
 function menuOpen(){
-    if(menuMiddle.style.opacity != "0"){
+    /*Open*/
+    if(menuMiddle.style.opacity != "0"){ 
         menuTop.style.animationName = "menu-animation-top";
         menuBottom.style.animationName = "menu-animation-bottom";
         menuMiddle.style.opacity = "0";
-
+        document.body.style.overflow ="hidden";
+        navSmall.style.display = "block";
+        console.log("Open");
+        
+    /*Closed*/
     }else{
-        menuMiddle.style.opacity = "1";
+        menuMiddle.style.opacity = "1"; 
         menuTop.style.animationName = "menu-animation-top-reverse";
         menuBottom.style.animationName = "menu-animation-bottom-reverse";
+        document.body.style.overflow ="scroll";
+        navSmall.style.display = "none";
+        console.log("Closed");
     }
 }
 
+
 menu.addEventListener("click", function(){menuOpen(); })
+
